a129c38822ee022fca658f272f500a937ce6c6ab
Rylan.Loong <SangUn.Yong@gmail.com>
2020-06-16 18:40:58 +0800

    search dns a record

--- 3program/powershell/DC-dns-chg-ip.ps1
+++ 3program/powershell/DC-dns-chg-ip.ps1
@@ -1,4 +1,4 @@
-$ARecord=Get-WmiObject -Namespace root\MicrosoftDNS -class microsoftdns_atype | ? {$_.OwnerName -eq "$using:name"};$ARecord.delete()
+$ARecord = Get-WmiObject -Namespace root\MicrosoftDNS -class microsoftdns_atype | ? { $_.OwnerName -eq "$using:name" }; $ARecord.delete()
 
 Add-DnsServerResourceRecordA -zonename 1hai.cn -name $using:name -ipaddress $using:ip
 dnscmd . /RecordAdd 1hai.cn $using:name A $using:ip
@@ -9,4 +9,27 @@ $dnsServerName = $containerName
 [string]$ip = "193.112.238.242"
 $dnsClass.CreateInstance(
 
+)
+Ivk-C 172.31.40.10 admin {
+    $ip = ((route print | findstr "0.0.0.0.*0.0.0.0") -replace "\s{2,}", " ").split()[4]
+    $sites = (Get-WmiObject -Namespace root\MicrosoftDNS -class microsoftdns_atype |
+        ? { $_.ownername -match ".1hai.cn$" })
+    $count = $sites.count
+    "`n--- $ip, Total Sites: $count"
+    $sites | % { ($_.ownername + "`t" + $_.recordData) }
+}
+Ivk-C 172.31.40.10 admin {
+    $ip = ((route print | findstr "0.0.0.0.*0.0.0.0") -replace "\s{2,}", " ").split()[4]
+    $sites = (Get-WmiObject -Namespace root\MicrosoftDNS -class microsoftdns_atype |
+        ? { $_.ownername -match ".1hai.cn$" })
+    $count = $sites.count
+    #$sites | % { ($_.ownername) }
+    #"`n--- $ip, Total Sites: $count"
+    $sites | % { ($_.ownername + "`t" + $_.recordData) }
+}
 
+$all = ($ssDns | % {
+        foreach ($site in $sqDns) {
+            $_ | ? { $site.split()[0] -contains $_.split()[0] }
+        }
+    })
