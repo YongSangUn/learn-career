2eadd9006a14d753b509d40636e79b6c1ceab827
Rylan.Loong <SangUn.Yong@gmail.com>
2020-07-03 19:05:51 +0800

    rm

--- get-ehi-config.ps1
+++
@@ -1,7 +0,0 @@
-(for /f "tokens=4 " %i in ('route print^|findstr "0.0.0.0.*0.0.0.0"') do @echo %i | findstr \. && hostname && whoami && echo ---------------- ) >> \\192.168.9.30\Scan\汇总数据\userConfig.txt
-
-echo hostname >> \\192.168.9.30\Scan\汇总数据\userConfig.txt
-for ($i = 1; $i -le ($a.count / 4); $i++) {
-    $ip = $a | Select -Index (4 * $i - 4)
-    $user = $a | select -Index (4 * $i - 2)
-}
