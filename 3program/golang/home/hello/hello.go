4c6b07ec2c169cf91d30be068bb084201c54d271
Rylan.Loong <sangun.yong@gmail.com>
2021-03-09 19:09:23 +0800

    Return greetings for multiple people

--- 3program/golang/home/hello/hello.go
+++ 3program/golang/home/hello/hello.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"example.com/greetings"
 	"fmt"
+	"log"
+
+	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("SangUn")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greeting: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Sangun", "Rylan", "Loong"}
+
+	// Request a greeting message.
+	message, err := greetings.Hellos(names)
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no errors was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
