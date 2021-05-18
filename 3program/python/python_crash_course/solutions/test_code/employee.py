998a0a3129af2de570f56553c73ff5d615f6f7e5
Rylan.Loong <sangun.yong@gmail.com>
2021-05-18 17:38:58 +0800

    chapter 11 soluttion done, part 1 done.

--- 3program/python/python_crash_course/solutions/test_code/employee.py
+++ 3program/python/python_crash_course/solutions/test_code/employee.py
@@ -1,44 +1,6 @@
-# chapter 11
-
-## 11-1..2
-
-```python
-# 11-1
-$ cat city_functions.py
 # -*- coding: utf-8 -*-
-
-def cityCountry(city, country, population=None):
-    if population:
-        message = f'{city}, {country} - population {population}'
-    else:
-        message = f'{city}, {country}'
-    return message.title()
-
-# -------------------------------
-
-# 11-2
-$ cat test_cities.py
-# -*- coding: utf-8 -*-
-
 import unittest
-from city_functions import cityCountry
 
-class CityTestCase(unittest.TestCase):
-
-    def testCityCountry(self):
-        message = cityCountry("shanghai", 'china')
-        self.assertEqual(message, 'Shanghai, China')
-
-    def testCityCountryPopulation(self):
-        message = cityCountry("shanghai", 'china', population=26000000)
-        self.assertEqual(message, 'Shanghai, China - Population 26000000')
-
-unittest.main()
-
-# 11-3
-# -*- coding: utf-8 -*-
-
-import unittest
 
 class Employee():
 
@@ -52,6 +14,7 @@ class Employee():
         if increment is None:
             increment = self._increment
         self.salary = self.salary + increment
+
 
 class TestEmplotee(unittest.TestCase):
 
@@ -68,5 +31,5 @@ class TestEmplotee(unittest.TestCase):
         self.employee.giveRaise(self.increment)
         self.assertEqual(self.employee.salary, self.increment)
 
+
 unittest.main()
-```
